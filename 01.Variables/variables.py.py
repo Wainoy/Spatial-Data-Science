641ce66734b88e665d8bac3f7ad02a6c584b554e
Wainoy <55856267+Wainoy@users.noreply.github.com>
2024-05-24 20:37:10 +0300

    reuploaded variables folder

---
+++ 01.Variables/variables.py.py
@@ -0,0 +1,12 @@
+price = 10
+price = 'ten'
+print (price)
+
+#Sample Exercise 2
+'''We’re checking in a new 20-year-old patient named John Smith at a given hospital. 
+What 3 variables do we need to store this information?'''
+name = 'John' 'Smith'
+age = 20
+is_new = True
+print (name)
+print(len(name))
