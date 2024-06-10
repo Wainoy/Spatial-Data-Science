bb71e87c22f8c1cef235b9214e2e20fb756376c9
Wainoy <55856267+Wainoy@users.noreply.github.com>
2024-06-10 10:18:47 +0300

    uploaded typeConversions2.py

---
+++ 04.Type_Conversions/typeConversions2.py
@@ -0,0 +1,9 @@
+#Type Conversion: asking someone their birth year and calculating their age
+birth_year = int(input('your birth year: '))
+age = 2024 - birth_year
+print('you are ',age,' years old')
+#Exercise 4: write a script to ask a user their weight and convert it to pounds
+weight = int(input('Your weight in kg: '))
+weight_pounds = weight*2.20
+print('your weight in pounds: ', weight_pounds)
+
