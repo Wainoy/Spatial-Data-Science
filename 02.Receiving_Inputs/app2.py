2fb1a04e4b2d3acf9b530a21e0b8a15234f99377
Wainoy <55856267+Wainoy@users.noreply.github.com>
2024-06-10 09:12:38 +0300

    updating the contents

---
+++ 02.Receiving_Inputs/app2.py
@@ -0,0 +1,10 @@
+#The input() function is used to get input from the user.
+name = input("Patient's name: ")
+age = input('Patient\'s age: ')
+print(name, 'is', age, 'years old')
+
+#Exercise 3: Ask a person's favorite color and print it on the screen.
+name = input('Your name: ')
+favorite_color = input('Your favorite color: ')
+print(name,'likes',favorite_color)
+
